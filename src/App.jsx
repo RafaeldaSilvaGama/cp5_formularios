a8e02a98b03345f06669f06d8335fa122affbd32
thais-ads <thaiss.spinoza@gmail.com>
2022-10-02 17:31:40 -0300

    ajustando a estrutura do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import './index.css';
-import Rodape from './components/rodape/Rodape';
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/home/Home';
-import Cabecalho from './components/cabecalho/Cabecalho';
 import PetCadastro from './components/petCadastro/PetCadastro';
+import './index.css';
 
 
 
@@ -12,13 +8,8 @@ export default function App() {
 
     return (
         <>
-
-            <Cabecalho />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-
-            <Rodape />
+            <PetCadastro/>
+            
         </>
     )
 }
--- src/components/petCadastro/PetCadastro.jsx
+++ src/components/petCadastro/PetCadastro.jsx
@@ -1,10 +1,33 @@
 import React from "react"
+import { DivEst,Botao } from "../../style/styled.js"
+
 
 export default function PetCadastro() {
-
     return (
-        <div>
-            
-        </div>
+
+        <DivEst>
+            <h1>Cadastro do Pet</h1>
+            <form action="">
+                <label htmlFor="">Nome Pet:</label><input type="text" name="" id="" placeholder="Nome Pet" />
+                    <br />
+                <label htmlFor="">Idade:</label><input type="text" name="" id="" placeholder="idade"/>
+                        <br />
+                <label htmlFor="">Raça:</label><input type="text" name="" id="" placeholder="raça"/>
+                    <br />
+                <label htmlFor="">tamanho:</label><input type="text" name="" id="" placeholder="pequeno,medio,grande"/>
+                    <br />
+                <label htmlFor="">Nome Dono:</label><input type="text" name="" id="" placeholder="Nome dono"/>
+                        <br />
+                <label htmlFor="">Telefone Dono:</label><input type="text" name="" id="" placeholder="(11)000000000"/>
+                            <br />
+                <label htmlFor="">Image do Pet:</label><input type="text" name="" id="" placeholder="anexar"/>
+                            <br />
+                <label htmlFor="">Observações:</label><input type="text" name="" id="" placeholder="descreva"/>
+                        <br />
+                        <br />
+                <Botao>enviar!</Botao>
+
+            </form>
+        </DivEst>
     )
 }
